a8a20b2594a93f28647537c92c2809e9317c45df
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-04-10 16:00:26 -0600

    style: :lipstick: UI improved
    repsonsive design

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,9 @@
-/* import AvailableOn from "../components/availableon";
-import Combo from "../components/combo"; */
+import AvailableOn from "../components/availableon";
 import Contact from "../components/Contact";
-/* import Coupons from "../components/coupons"; */
 import FAQ from "../components/faq";
 import Kings from "../components/Kings";
 import Location from "../components/location";
-/* import Menu from "../components/menu";
-import Order from "../components/Order"; */
 import Parallax from "../components/parallax";
-/* import Hero2 from "../components/temp/Hero2"; */
 /* import Promos from "../components/Promos"; */
 /* import Social from "../components/Social"; */
 import Stores from "../components/Stores";
@@ -19,12 +14,7 @@ import styles from "../styles/landing.module.css";
 function Landing() {
   return (
     <div className={styles.landing}>
-      {/* <Hero2 />
-      <Combo />
-      <Menu />
-      <Coupons />
-      <Order />
-      <AvailableOn /> */}
+      <AvailableOn />
       <Stores />
       <Location />
       <Testimonials />
