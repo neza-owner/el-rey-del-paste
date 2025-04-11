d55a66482199929248394c73220c5c55c0290800
elvisscochito <elvirodominguezsoriano@outlook.com>
2025-04-10 22:51:23 -0600

    fixing coupons page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,29 +1,13 @@
-import AvailableOn from "../components/availableon";
-import Contact from "../components/Contact";
-import FAQ from "../components/faq";
-import Kings from "../components/Kings";
-import Location from "../components/location";
-import Parallax from "../components/parallax";
+import Coupons from "../components/coupons";
 /* import Promos from "../components/Promos"; */
 /* import Social from "../components/Social"; */
-import Stores from "../components/Stores";
-import Testimonials from "../components/testmonials";
 /* import Jobs from "./Jobs"; */
 import styles from "../styles/landing.module.css";
 
 function Landing() {
   return (
     <div className={styles.landing}>
-      <AvailableOn />
-      <Stores />
-      <Location />
-      <Testimonials />
-      <Kings />
-      <Parallax />
-      {/* <Social /> */}
-      <Contact />
-      {/* <Jobs /> */}
-      <FAQ />
+      <Coupons />
     </div>
   );
 }
